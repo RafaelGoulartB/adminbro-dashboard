d7565964b056e1a6b79c07ebf28a21db62b1f964
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-04 10:21:26 -0300

    refact: update with eslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const AdminBro = require("admin-bro")
-const AdminBroExpress = require("@admin-bro/express")
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
 const AdminBroMongoose = require('@admin-bro/mongoose')
 const Project = require('./db/Project')
 
@@ -13,28 +13,28 @@ const adminBroOptions = new AdminBro({
       resource: Project,
       options: {
         properties: {
-          description: { type: "richtext" },
+          description: { type: 'richtext' },
           created_at: {
-            isVisible: { edit: false, list: true, show: true, filter: true },
-          },
-        },
-      },
-    },
+            isVisible: { edit: false, list: true, show: true, filter: true }
+          }
+        }
+      }
+    }
   ],
   locale: {
     translations: {
       labels: {
-        Project: "My Projects",
-      },
-    },
+        Project: 'My Projects'
+      }
+    }
   },
-  rootPath: "/admin",
+  rootPath: '/admin'
 })
 
 const router = AdminBroExpress.buildRouter(adminBroOptions)
 
 // Server
-const express = require("express")
+const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
@@ -44,10 +44,10 @@ app.use(adminBroOptions.options.rootPath, router)
 const run = async () => {
   await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ghmix.gcp.mongodb.net/adminbro-db?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    useUnifiedTopology: true
+  })
 
-  await app.listen(8080, () => console.log("Server started at http://localhost:8080/"));
+  await app.listen(8080, () => console.log('Server started at http://localhost:8080/'))
 }
 
 run()

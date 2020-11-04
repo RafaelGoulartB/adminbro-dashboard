4c224f8fcdb78fa10e960b7d6232c049fc1145db
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-04 11:10:42 -0300

    refact: separete adminbro-option in a file

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,25 @@
 require('dotenv').config()
 
-const AdminBro = require('admin-bro')
+const express = require('express')
+const mongoose = require('mongoose')
 const AdminBroExpress = require('@admin-bro/express')
-const AdminBroMongoose = require('@admin-bro/mongoose')
-const Project = require('./db/Project')
 
-AdminBro.registerAdapter(AdminBroMongoose)
-
-const adminBroOptions = new AdminBro({
-  resources: [
-    {
-      resource: Project,
-      options: {
-        properties: {
-          description: { type: 'richtext' },
-          created_at: {
-            isVisible: { edit: false, list: true, show: true, filter: true }
-          }
-        }
-      }
-    }
-  ],
-  locale: {
-    translations: {
-      labels: {
-        Project: 'My Projects'
-      }
-    }
-  },
-  rootPath: '/admin'
-})
+const adminBroOptions = require('./adminbro-options')
 
 const router = AdminBroExpress.buildRouter(adminBroOptions)
 
-// Server
-const express = require('express')
-const mongoose = require('mongoose')
 const app = express()
 
 app.use(adminBroOptions.options.rootPath, router)
 
-// Run App
 const run = async () => {
   await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ghmix.gcp.mongodb.net/adminbro-db?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
 
-  await app.listen(8080, () => console.log('Server started at http://localhost:8080/'))
+  await app.listen(8080)
 }
 
 run()

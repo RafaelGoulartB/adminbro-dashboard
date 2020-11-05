be8941fa364bad8cc25266d9c8467a27baac1630
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-05 12:04:44 -0300

    feat: add custom dashboard component

--- .adminbro/.entry.js
+++ .adminbro/.entry.js
@@ -1,1 +1,3 @@
 AdminBro.UserComponents = {}
+import Component1 from '../src/components/my-dashboard-component'
+AdminBro.UserComponents.Component1 = Component1
---
+++ src/components/my-dashboard-component.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@admin-bro/design-system'
+
+const Dashboard = () => {
+  return (
+    <Box variant="grey">
+      <Box variant="white">
+        Go to sidebar to manage the system
+      </Box>
+    </Box>
+  )
+}
+
+export default Dashboard

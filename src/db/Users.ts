0e5ceabb84a11c908d702354ee1e6d48b3eba947
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-05 11:15:40 -0300

    feat: add timestamps fields in users

--- src/db/Users.ts
+++ src/db/Users.ts
@@ -13,11 +13,9 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     require: true
-  },
-  createdAt: {
-    default: Date.now(),
-    type: Date
   }
+}, {
+  timestamps: true
 })
 
 UserSchema.pre('save', async function (this: typeof UserSchema.pre, next: any) {
